1fd37117cdf6f6d22d7b1594785bb600d2a267f7
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-03-05 19:52:15 -0300

    operadores aritmeticos e Logicos

---
+++ segundo.dart
@@ -0,0 +1,19 @@
+void main(){
+  //Operações aritmeticas com dart, dart possui os operadores aritmeticos:
+  //Soma +,subtração -, multiplicação *, divisao /, resto de divisão %
+  double num1 = 10.0;
+  double num2 = 15;
+  double result = num1+num2;
+  result+=1; //result=result+1
+  result++; //e dai por diante
+  print(result);
+
+  //Operações Logicos com dart,
+  //comparadores,Seguem o padrão de todas as linguagem >,<,>=,<=,==,!=, todos retornam true ou false dependendo da pergunta
+  bool ehPar = ((2%2)==0);
+  print(ehPar);
+  ehPar = ((2%2)!=0);
+  print(ehPar);
+
+
+}

b3cfeeb9ddc1c116b118b2c3b7b23ea6a8d55737
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-03-06 20:55:29 -0300

    addicionando imagem ao projeto

--- Sobre-Fluter/contapessoasrestaurante/lib/main.dart
+++ Sobre-Fluter/contapessoasrestaurante/lib/main.dart
@@ -1,20 +1,41 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(MaterialApp(
       title: "Contador de Pessoas",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "Pessoa:0",
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "Pode Entrar",
-            style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic,
-                fontSize: 30.0),
+          Image.asset("images/restaurant.jpg",fit: BoxFit.cover,height: 1000.0,),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoa:0",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(child: Text("+1",style: TextStyle(fontSize: 40,color: Colors.white)),onPressed: (){},),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(child: Text("+1",style: TextStyle(fontSize: 40,color: Colors.white)),onPressed: (){},),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode Entrar",
+                style: TextStyle(color: Colors.white, fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
           )
         ],
-      )));
+      )
+  ));
 }

d7bd628ee54147d14e0c3234df878b17fd2c4db5
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-03-06 10:42:05 -0300

    modificadores satic,const e final

---
+++ oitavo.dart
@@ -0,0 +1,14 @@
+class Variaveis{
+  static int vezesClicado=0;
+}
+void main(){
+  //modificadores Static ,Final, Const no dart
+  //Usada quando vc quer fazer uma classe com constantes e não quer ter que instanciar ess classe apena para usar a variavel etc
+  Variaveis.vezesClicado=1;
+  const pi = 3.14;
+  print(pi);
+  // final Pessoa p1 = Pessoa();
+
+
+
+}

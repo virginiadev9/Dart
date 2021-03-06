6e22f0e4034e25f083b81c7454a91d0b83c79a12
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-03-05 21:46:06 -0300

    funçoes anonimas

--- quinto.dart
+++ quinto.dart
@@ -6,9 +6,25 @@ void main(){
   int result = somaDoisnumeros(num1,num2);
   print(result);
   print(result =somaDoisnumeros2(num1, num2));
+  criarBotao("SAIR",cor: "PRETO",largura: 20.0);
+
+  criarBotao2("SAIR",criadoFun,cor: "PRETO",largura: 20.0);
+
+  criarBotao3("SAIR",(){
+    print("boato");
+  });
+
+
+
+
+
+
+
 
 
 }
+
+
 void printIntro(){
   print("Seja bem vindo");
   
@@ -18,3 +34,26 @@ int somaDoisnumeros(int x,int y){
 }
 //Funçoes com corpo so de 1 linha
 int somaDoisnumeros2(int x,int y)=> x+y;
+
+// Funções com parametros opcionais
+void criarBotao(String texto,{String cor="blue", double largura=0.0}){  //coloca os parametros opcionais entre chaves, Tem que definir valor Default na Escritura/ou require
+  print(texto);
+  print(cor);
+  print(largura);
+}
+void criadoFun(){
+  print("criado");
+}
+//Funçoes anonimas
+void criarBotao2(String texto,Function criadoFun,{String cor="blue", double largura=0.0}){//passo outrafunção
+  print(texto);
+  print(cor);
+  print(largura);
+  criadoFun();
+}
+void criarBotao3(String texto,Function criadoFun,{String cor="blue", double largura=0.0}){//passo outrafunção
+  print(texto);
+  print(cor);
+  print(largura);
+  criadoFun();
+}

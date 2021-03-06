c342109f8c0089b5bce8b4b52d7014656e3c2e80
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-03-06 10:11:28 -0300

    classes e heranças

--- setimo.dart
+++ setimo.dart
@@ -7,7 +7,6 @@ class Pessoa{
   //construtor
   Pessoa(this._nome,this._idade,this._altura);
 
-
   //metodos gets e sets
   String get nome{
     return this._nome;
@@ -31,14 +30,31 @@ class Pessoa{
   }
 
 }
-class Cliente{
-  String _cpf;
-  Cliente(this._cpf);
+class  Cliente extends Pessoa{
+  String _cPF;
+
+  Cliente(this._cPF, String nome,int idade, double altura) : super(nome,idade,altura); //obsever como passo para o construtor
   void comprar(){
     print("comprar");
   }
+  String get cPF{
+    return this._cPF;
+  }
+  set cPF(String cpf){
+    this._cPF=cpf;
+  }
 }
 void main(){
+  Cliente x1 = new Cliente("11600808441","julho", 26, 1.86);
+  print(x1.nome);
+  print(x1.idade);
+  print(x1.cPF);
+
+  x1._cPF = "118.009.98-31";
+  x1._altura = 1.74;
+  print(x1.nome);
+  print(x1.idade);
+  print(x1.cPF);
 
 
 

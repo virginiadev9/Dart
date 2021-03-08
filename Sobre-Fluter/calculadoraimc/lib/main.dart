2a35be9f64c14c65eba99def1c39dfc11de50256
“1cbzin-95” <clodoaldo.brtp4@gmail.com>
2021-03-07 23:24:33 -0300

    finalizando segunda app

--- Sobre-Fluter/calculadoraimc/lib/main.dart
+++ Sobre-Fluter/calculadoraimc/lib/main.dart
@@ -17,40 +17,40 @@ class _HomeState extends State<Home> {
   TextEditingController heightControler = TextEditingController();
   String _infoText = "Informe seus dados";
 
-  void _resetFields(){
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _resetFields() {
+    weightControler.text = "";
+    heightControler.text =
+        ""; //não precisa dar setState nos controles pq apartir do momente que informo que eles sao controladores eles ja tem setSate
     setState(() {
-      weightControler.text="";
-      heightControler.text="";
       _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();    // ADICIONE ESTA LINHA!
+
     });
   }
 
-  void _calculate(){
+  void _calculate() {
     setState(() {
       double weight = double.parse(weightControler.text);
-      double height = double.parse(heightControler.text)/100;
-      double imc = weight/(height*height);
-      if(imc<18.6){
-        _infoText="Abaixo do peso(${imc.toStringAsPrecision(2)})";
-      }
-      else if(imc >=18.6 && imc <24.9) {
+      double height = double.parse(heightControler.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do peso(${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
         _infoText = "Peso Ideal(${imc.toStringAsPrecision(2)})";
-      }
-      else if(imc >=24.9 && imc <29.9){
-        _infoText="Levemente acima do peso(${imc.toStringAsPrecision(2)})";
-      }
-      else if(imc >=29.9 && imc <34.9){
-        _infoText="Obesidade Grau 1(${imc.toStringAsPrecision(2)})";
-      }
-      else if(imc >=34.9 && imc <29.9){
-        _infoText="Obesidade Grau 2(${imc.toStringAsPrecision(2)})";
-      }
-      else if(imc>=40){
-        _infoText="Obesidade  Grau 3(${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso!(${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau 1(${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 34.9 && imc < 29.9) {
+        _infoText = "Obesidade Grau 2(${imc.toStringAsPrecision(2)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade  Grau 3(${imc.toStringAsPrecision(2)})";
       }
     });
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,51 +68,68 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(
-                Icons.person_outline,
-                size: 120.0,
-                color: Colors.green,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Peso (kg)",
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-                controller: weightControler,
-              ),
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                    labelText: "Altura (cm)",
-                    labelStyle: TextStyle(color: Colors.green)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-                controller: heightControler,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                child: Container(
-                  height: 50.0,
-                  child: RaisedButton(
-                    onPressed: _calculate,
-                    child: Text(
-                      "Calcular",
-                      style: TextStyle(color: Colors.green, fontSize: 25.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
+                  size: 120.0,
+                  color: Colors.green,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Peso (kg)",
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  controller: weightControler,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira seu Peso!";
+                    }
+                  },
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                  controller: heightControler,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira sua Altura!";
+                    }
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                  child: Container(
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: (){
+                        if(_formKey.currentState.validate()){
+                          _calculate();
+                        }
+                      },
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(color: Colors.green, fontSize: 25.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Text(
-                _infoText,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.green, fontSize: 25.0),
-              )
-            ],
+                Text(
+                  _infoText,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                )
+              ],
+            ),
           ),
         )); //um widget que facilita a vida para colocar outros widget do material designer
   }
